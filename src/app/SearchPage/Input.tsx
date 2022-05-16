5ef779b9dc4b9bafb0ec417361e791378a7da476
borshhevskijj <borshhevskijj98@mail.ru>
2022-05-16 15:12:25 +0300

    added `like` feature

--- src/app/SearchPage/Input.tsx
+++ src/app/SearchPage/Input.tsx
@@ -7,14 +7,14 @@ import { handToggle } from '../Search';
 import SearchPage from './SearchPage';
 
 
+const lastSearchRequest = localStorage.getItem('lastSearchRequest')//получаем последний запрос
 
 export default function Input(props:any) {
-  const lastSearchRequest = localStorage.getItem('lastSearchRequest')//получаем последний запрос
   const [inputValue, setInputValue]= useState <string | undefined> (lastSearchRequest??'как написать свой ютуб?') // инпут
   const [toggle,setToggle] = useState<boolean>(JSON.parse(localStorage.getItem('toggle') as string) || false)
-
   const debouncedInput = useDebounce(inputValue as string,500)// выводит текст с задержкой
   const inputValueToUrl = (inputValue as string).replace(/ /gim,'%20') // для url адреса убирает пробелы и заменяет их символом
+
 
       useEffect(()=>{
         setToggle(JSON.parse(localStorage.getItem('toggle') as string))
--- src/app/SearchPage/Videos.tsx
+++ src/app/SearchPage/Videos.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {useDispatch} from 'react-redux'
 import {addToFavorites} from '../../store/SearchSlice'
 import cl from '../../styles/search.module.css'
 import { IvideoItems } from '../../interfaces/searchResult'
+import { handToggle } from '../Search'
+
+// const notFavorite = {
+//     display: 'inlineBlock',
+//     padding: '0.4em 1.4em',
+//     textDecoration:' none',
+//     color: '#67c5ff',
+//     border: 'none',
+//     borderRadius: '3px',
+//     transition: '.4s',
+//     backgroundColor: '#1390E5',
+// }
+// const alreadyFavorite = {
+//     display: 'inlineBlock',
+//     padding: '0.4em 1.4em',
+//     textDecoration:' none',
+//     color: '#000000',
+//     border: 'none',
+//     borderRadius: '3px',
+//     transition: '.4s',
+//     backgroundColor: '#10fc6f',
+// }
 
 export default function Videos(props:any) {
-    const dispatch = useDispatch();
+const dispatch = useDispatch();
+const [likedVideo,setLikedVideo]= useState<string[]>([]) 
+const [toggle,setToggle]= useState(false)
+
+    useEffect(() => {
+    const likedVideos =():any=>{
+    return props.videos?.items 
+    .map((item:IvideoItems) => item.id.videoId)
+    .filter((videoId:any)=> videoId !== undefined && `https://www.youtube.com/embed/${videoId}` ===  localStorage.getItem(`https://www.youtube.com/embed/${videoId}`))}
+
+    return setLikedVideo(likedVideos)
+ 
+    }, [toggle,setLikedVideo,props.videos])
+
   return (
         <>
             {
@@ -15,14 +50,25 @@ export default function Videos(props:any) {
                     className={cl.iFrameYouTubeVideo}>
                         <iframe src={`https://www.youtube.com/embed/${item.id.videoId}`} title='{video.id}' frameBorder="0" allowFullScreen></iframe>
                         <p>{item.snippet.channelTitle}</p>
+
                         <button 
-                        onClick={()=> dispatch(addToFavorites(`https://www.youtube.com/embed/${item.id.videoId}`))} 
-                        className={cl.addToFavorite}
-                        >LIKE
+                        onClick={
+                            ()=> dispatch(addToFavorites(`https://www.youtube.com/embed/${item.id.videoId}`))
+                            && handToggle(setToggle,toggle)
+                        } 
+                        className={
+                        likedVideo?.includes(item.id.videoId)
+                        ? cl.notFav
+                        : cl.fav
+                        }
+                        > { likedVideo?.includes(item.id.videoId)
+                            ? 'В избранное'
+                            : 'Уже в избранном'
+                            }
                         </button>
                     </div>
                 )
-                : <div>Ничего не найдено</div>
+                : <div>Ничего не найдено</div> // не работает
             }
         </>
     )
